5d6d3424981ddedb1deb73db6b0acb219a6e174e
Micol <micolpa08@gmail.com>
2021-03-16 15:18:21 -0400

    quitar enlace admin

--- frontend/views/admin/index.php
+++ frontend/views/admin/index.php
@@ -2,7 +2,7 @@
 	
 	<div class="row mt-5 pt-5">
 		<div class="col-md-4">
-			<a href="/frontend/web/users" class="no-link">
+			<a href="/frontend/web/user" class="no-link">
 				<div class="bg-darkblue mt-5 text-white text-center pt-5 pb-5">
 					<i class="fas fa-users fa-2x"></i>
 					<p class="text-center text-white mt-4 h4 font-weight-light">USUARIOS</p>
@@ -18,7 +18,7 @@
 			</a>
 		</div>
 		<div class="col-md-4">
-			<a href="/frontend/web/anuncios" class="no-link">
+			<a href="#" class="no-link">
 				<div class="bg-darkblue mt-5 text-white text-center pt-5 pb-5">
 					<i class="fas fa-users fa-2x"></i>
 					<p class="text-center text-white mt-4 h4 font-weight-light">ANUNCIOS</p>
