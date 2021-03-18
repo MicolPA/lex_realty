2c57569880c9c7109029e55de2117bc43e7419e0
Micol <micolpa08@gmail.com>
2021-03-17 20:12:17 -0400

    actualizando css

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=14',
+        'css/site.css?v=15',
         'css/bootstrap.css',
         'css/all.min.css',
     ];
