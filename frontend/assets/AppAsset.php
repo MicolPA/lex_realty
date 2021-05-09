afd39e5bfe0be205685ba3abf69034cd5ccb75a3
Micol <micolpa08@gmail.com>
2021-05-09 15:29:07 -0400

    some fix

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=23',
+        'css/site.css?v=24',
         'css/bootstrap.css',
         'css/all.min.css',
     ];
