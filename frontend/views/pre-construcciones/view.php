fbec07ea57904fccec163d40e1f1e6e8f7ff0f87
Micol <micolpa08@gmail.com>
2021-08-01 20:05:46 -0400

    btn hover white

--- frontend/views/pre-construcciones/view.php
+++ frontend/views/pre-construcciones/view.php
@@ -36,6 +36,8 @@ $prev_propiedad = \frontend\models\PreConstrucciones::find()->where(['>', 'id', 
     #carousel-thumbs{
         width: 100% !important;
     }
+
+   
 </style>
 <!-- <style>
     .swal-modal{
@@ -196,11 +198,11 @@ $prev_propiedad = \frontend\models\PreConstrucciones::find()->where(['>', 'id', 
                 <!-- <a href="/frontend/web/propiedades/enviar-propuesta?id=<?= $model->id ?>&user_id=<?= $model->user_id ?>&propiedad=0" class="btn-block text-success text-center p-0 pt-2 pb-2 btn btn-outline-success mt-4 h6">ENVIAR PROPUESTA</a> -->
                 <!-- <a href="/frontend/web/propiedades/enviar-propuesta?id=<?= $model->id ?>&user_id=<?= $model->user_id ?>&propiedad=0" class="btn-block text-success text-center p-0 pt-2 pb-2 btn btn-outline-success mt-4 h6">ENVIAR PROPUESTA</a> -->
                 <!-- <a href="/frontend/web/propiedades/contactar-agente?id=<?= $model->id ?>&user_id=<?= $model->user_id ?>&type=2&propiedad=0" class="btn-block btn btn-outline-dark text-center p-0 pt-2 pb-2 h6">CONTACTAR UN AGENTE</a> -->
-                <a href="/frontend/web/desarrolladores/index?id=<?= $model->desarrollador->id ?>&stars=1" class="btn-block text-success text-center p-0 pt-2 pb-2 btn btn-outline-success h6">DESARROLLADOR</a>
+                <a href="/frontend/web/desarrolladores/index?id=<?= $model->desarrollador->id ?>&stars=1" class="btn-block text-center p-0 pt-2 pb-2 btn btn-outline-success h6">DESARROLLADOR</a>
 
-                <a href="/frontend/web/propiedades/ver-dictamen?id=<?= $model->id ?>&propiedad_check=0" class="btn-block text-dark text-center p-0 pt-2 pb-2 btn btn-outline-dark h6" target='_blank'>DESCARGAR DICTAMEN</a>
+                <a href="/frontend/web/propiedades/ver-dictamen?id=<?= $model->id ?>&propiedad_check=0" class="btn-block text-center p-0 pt-2 pb-2 btn btn-outline-dark h6" target='_blank'>DESCARGAR DICTAMEN</a>
 
-                <a href="/frontend/web/tasas-hipotecarias" class="btn-block text-primary text-center p-0 pt-2 pb-2 btn btn-outline-primary h6">TASAS HIPOTECARIAS</a>
+                <a href="/frontend/web/tasas-hipotecarias" class="btn-block text-center p-0 pt-2 pb-2 btn btn-outline-primary h6">TASAS HIPOTECARIAS</a>
 
                 <div class="row mt-5 pt-5">
                     <div class="col-md-6 col-xs-6 col-sm-6 text-right mt-4" style="max-width: 50% !important">
